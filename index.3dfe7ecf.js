dcaaf69598adf5022870d27d4d09e8cd8e6ef048
Yanna-Trofimova <111576903+Yanna-Trofimova@users.noreply.github.com>
2023-02-01 14:32:52 +0000

    Deploying to gh-pages from @ Yanna-Trofimova/goit-js-hw-10@38cd34bed4dc58bab1fc747a96e2c08afb2c9f8f 🚀

--- index.3dfe7ecf.js
+++
@@ -1,2 +0,0 @@
-({getCountries:function(n){return fetch("${ENDPOINT}${name}?fields=name,capital,population,flags,languages").then((n=>n.json()))}}).getCountries("Japan").then(console.log);
-//# sourceMappingURL=index.3dfe7ecf.js.map
--- index.d152a18b.js
+++
@@ -1,2 +0,0 @@
-fetch("${ENDPOINT}${name}?fields=name,capital,population,flags,languages").then((function(n){return n.json()})).then(console.log);
-//# sourceMappingURL=index.d152a18b.js.map
