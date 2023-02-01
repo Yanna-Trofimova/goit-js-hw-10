38cd34bed4dc58bab1fc747a96e2c08afb2c9f8f
Yanna-Trofimova <111576903+Yanna-Trofimova@users.noreply.github.com>
2023-02-01 16:32:27 +0200

    upd

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,13 +2,12 @@ const ENDPOINT = "https://restcountries.com/v3.1/name/";
 
 
 function getCountries(name) {
-    return fetch('${ENDPOINT}${name}?fields=name,capital,population,flags,languages')
+    return fetch(`${ENDPOINT}${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
             // console.log(response.json());
             return response.json();
         });
-    // .then(country => {console.log(country);
-    // })
+        // .then(data => data.country);
     // .catch(error => {
     //     console.log(error);
     // });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
+import debounce from 'lodash.debounce';
+
 import './css/styles.css';
 import API from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 
-API.getCountries('Japan').then(console.log);
+// API.getCountries('Japan').then(console.log);
+
+const input = document.getElementById("search-box");
+const divContainer = document.querySelector(".country-info");
+
+input.addEventListener(
+  "input", debounce (onInputSearch, DEBOUNCE_DELAY )
+);
 
 
+function onInputSearch(e) {
+    e.preventDefault();
+    const inputValue = e.target.value;
+    console.log(inputValue);
+    // API.getCountries = inputValue;
+    // API.getCountries(inputValue).then(renderCountryCard);
+    
+}
 
+function creatCard({ name, capital, population, flags, languages }) {
+    // const langCard = languages.map(language => language.name).join(", ");
+    // const langCard = Object.values(languages).join(", ");
 
+    return `
+    <div class="info-card">
+    
+      <img src="${flags}" alt="flag" class="info-card_flag" width="100" heigth="100">
+      <h1 class="info-card__name">${name}</h1>
+      <p class="info-card_capital">Capital: ${capital}</p>
+      <p class="info-card_popul">Population: ${population}</p>
+      <p class="info-card_lang">Languages: ${languages}</p>
+    </div>
+    `;
+}
 
-// console.log(r);
+function renderCountryCard(country) {
+    const markup = creatCard(country);
+    divContainer.innerHTML = markup;
+}
+
+API.getCountries(onInputSearch).then(renderCountryCard);
+
